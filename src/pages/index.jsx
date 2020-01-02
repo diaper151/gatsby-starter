8b29e55256845d049e9721b76eeb5fa54432ff0c
Morris Tai <diaper151@gmail.com>
2020-01-03 00:41:28 +0800

    website test react

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -276,21 +276,7 @@ const Index = () => (
           <a href="https://github.com/diaper151">Github Repository</a>.
         </Footer>
       </Content>
-      <Divider speed={0.1} offset={4}>
-        <UpDown>
-          <SVG icon="upDown" className={hidden} width={8} fill={colors['grey-darkest']} left="70%" top="20%" />
-          <SVG icon="triangle" width={8} stroke={colors['grey-darkest']} left="25%" top="5%" />
-        </UpDown>
-        <UpDownWide>
-          <SVG icon="triangle" width={12} stroke={colors.white} left="95%" top="50%" />
-          <SVG icon="circle" width={6} fill={colors.white} left="85%" top="15%" />
-          <SVG icon="upDown" className={hidden} width={8} fill={colors['grey-darkest']} left="45%" top="10%" />
-        </UpDownWide>
-        <SVG icon="circle" width={6} fill={colors.white} left="4%" top="20%" />
-        <SVG icon="circle" width={12} fill={colors['grey-darkest']} left="70%" top="60%" />
-        <SVG icon="box" width={12} fill={colors['grey-darkest']} left="20%" top="30%" />
-        <SVG icon="hexa" width={8} stroke={colors['grey-darkest']} left="80%" top="70%" />
-      </Divider>
+
     </Parallax>
   </React.Fragment>
 );
